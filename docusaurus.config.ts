8419299ca744e3c2c2844892692503bd8bb655cd
mallusrgreatv2 <mallusrgreat@gmail.com>
2025-01-10 22:10:32 +0530

    push

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'SSLC Study Material',
+  title: 'SSLC Study Materials',
   tagline: 'A collection of the best materials you\'ll ever find.',
   favicon: 'img/favicon.ico',
 
